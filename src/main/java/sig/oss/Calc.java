312749cc3663ea640a766e3fc4fefc72843576d2
JungJinYang <zxv0105@naver.com>
2016-08-24 15:42:55 +0900

    Update Calc.java

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,16 +1,16 @@
 public class Calc{
+
+	public int r=0;
 	public int add(int x,int y){//add method
 	return x+y;}
 	public int sub(int x,int y){//sub method
 	return x-y;
 	}
-
-
 	public int mul(int x,int y){//mul method1 2
 	return x*y;
 	}	
+	public void dec(int d){//add method
+	r-=d;
+	}
 
-
-~
-~
 
