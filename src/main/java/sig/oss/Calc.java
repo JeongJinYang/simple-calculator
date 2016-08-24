4ac655bf25c24acc98a4b81781486f7319eea00b
Jung, JinYang <zxv0105@naver.com>
2016-08-24 13:59:13 +0900

    add multi func and its test code to Calc and CalcTest

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,10 +1,14 @@
 public class Calc{
 	public int add(int x,int y){//add method
-	return x+y;
+	return x+y;}
 	public int sub(int x,int y){//sub method
 	return x-y;
 	}
-}
+
+
+	public int mul(int x,int y){//mul method
+	return x*y;
+	}	
 
 
 ~
--- src/test/java/sig/oss/CalcTest.java
+++ src/test/java/sig/oss/CalcTest.java
@@ -12,4 +12,9 @@ public void testSub(){
 Calc c = new Calc();
 assertEquals(30, c.Sub(10,20));
 }
+public void testMul(){
+Calc c = new Calc();
+assertEquals(30, c.Mul(10,20));
+}
+
 }
